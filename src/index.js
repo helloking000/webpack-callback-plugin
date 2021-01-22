e3737183b2c4ed23f8a227c7e0d1c6f983eb8511
tengyun.wty <tengyun.wty@alibaba-inc.com>
2021-01-22 15:07:57 +0800

    增加处理:传入命令为空

--- src/index.js
+++ src/index.js
@@ -9,19 +9,20 @@ class WebpackCallbackPlugin {
     }
 
     apply(compiler) {
-        // compiler.hooks.afterEmit.tap(
         compiler.hooks.done.tap( // 参考 webpack dev server 在 done 时，通过 socket 给浏览器发信息
             'MyPlugin',
             (compilation) => {
-                // tnpm run push-device && adb logcat | grep FliggyJsPageUtils
-                exec(this.command, (error, stdout, stderr) => {
-                    if (error) {
-                      console.error(`执行的错误: ${error}`);
-                      return;
-                    }
-                    console.log(`stdout: ${stdout}`);
-                    console.error(`stderr: ${stderr}`);
-                });
+                if (this.command) { // 防止会传入空命令
+                    exec(this.command, (error, stdout, stderr) => {
+                        if (error) {
+                          console.error(`执行的错误: ${error}`);
+                          return;
+                        }
+                        console.log(`stdout: ${stdout}`);
+                        console.error(`stderr: ${stderr}`);
+                    });
+                }
+
             }
         );
     }
